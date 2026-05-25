08d16effc9baf125edd2b8a0495b403929cc3dbe
noviarn <noviarhmns@gmail.com>
2026-05-25 11:39:41 +0800

    test

---
+++ CH3_8AM_TeachersGood/Views/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  CH3_8AM_TeachersGood
+//
+//  Created by Novia Rahman Nisa on 25/05/26.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
