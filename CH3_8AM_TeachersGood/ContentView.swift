f59beee05fbdbb7fd9e78514eb03e49383e931bd
Syauqi Auliya (Cho) <syauqido@gmail.com>
2026-05-25 11:35:34 +0800

    Testing

--- CH3_8AM_TeachersGood/ContentView.swift
+++ CH3_8AM_TeachersGood/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Challenge 3")
+            Text("Challenge 3 Cho's Tests")
         }
         .padding()
     }
