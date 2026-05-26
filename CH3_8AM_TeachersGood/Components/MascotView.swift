248564066a39d39fe3a716b9e8423b75d10cc8b6
Syauqi Auliya (Cho) <syauqido@gmail.com>
2026-05-26 08:57:49 +0800

    Thingy mascot reusable component

---
+++ CH3_8AM_TeachersGood/Components/MascotView.swift
@@ -0,0 +1,19 @@
+//
+//  MascotView.swift
+//  CH3_8AM_TeachersGood
+//
+//  Created by Syauqi Auliya M on 26/05/26.
+//
+
+import SwiftUI
+
+struct MascotView: View {
+    
+    var body: some View {
+        Image("Thingy")
+    }
+}
+
+#Preview {
+    MascotView()
+}
