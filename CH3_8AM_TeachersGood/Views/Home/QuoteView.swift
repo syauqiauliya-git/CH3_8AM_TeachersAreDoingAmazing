257814a924aecf255ca215253519a1328b124fc9
noviarn <noviarhmns@gmail.com>
2026-05-26 09:19:58 +0800

    added the chat button

--- CH3_8AM_TeachersGood/Views/Home/QuoteView.swift
+++ CH3_8AM_TeachersGood/Views/Home/QuoteView.swift
@@ -9,7 +9,44 @@ import SwiftUI
 
 struct QuoteView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Button {
+                    // action
+                } label: {
+                    Image(systemName: "person")
+                        .foregroundStyle(Color.blue) // color of this will be changed
+                }
+                .frame(width: 50, height: 50)
+                .frame(maxWidth: .infinity, alignment: .topTrailing)
+                .buttonBorderShape(.circle)
+                .buttonStyle(.glass)
+                .controlSize(ControlSize.large)
+                Spacer()
+                Text("“I want to change the future by educating younger generations.”")
+                    .font(.system(size: 40, design: .serif))
+                HStack {
+                    Spacer()
+                    Text("- Lorem")
+                        .font(.system(size: 20, design: .serif))
+                        .italic(true)
+                }
+                .padding(.trailing, 25)
+                Spacer()
+                NavigationLink {
+                    // add later
+                } label: {
+                    Circle()
+                        .fill(Color(red: 35/255, green: 39/255, blue: 144/255))
+                        .frame(width: 50, height: 50)
+                        .overlay(Image(systemName: "ellipsis.message.fill"))
+                        .font(.system(size: 20))
+                        .foregroundColor(.white)
+                }
+                .frame(maxWidth: .infinity, alignment: .bottomTrailing)
+            }
+            .padding(20)
+        }
     }
 }
 
