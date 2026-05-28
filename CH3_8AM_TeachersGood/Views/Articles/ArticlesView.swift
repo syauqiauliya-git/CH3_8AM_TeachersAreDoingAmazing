369bc791875e0c4710cff82f3abf35fb67a31a08
noviarn <noviarhmns@gmail.com>
2026-05-28 19:20:22 +0800

    articles folder and main screen created

---
+++ CH3_8AM_TeachersGood/Views/Articles/ArticlesView.swift
@@ -0,0 +1,18 @@
+//
+//  ArticlesView.swift
+//  CH3_8AM_TeachersGood
+//
+//  Created by Novia Rahman Nisa on 28/05/26.
+//
+
+import SwiftUI
+
+struct ArticlesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArticlesView()
+}
