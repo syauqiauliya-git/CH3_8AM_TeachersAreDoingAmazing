b5734b0c75b6b9a98a1acd55d88b025deb5ea26e
noviarn <noviarhmns@gmail.com>
2026-06-07 23:43:42 +0800

    fixed some profile hifi

--- CH3_8AM_TeachersGood/Views/Profile/EditNameView.swift
+++ CH3_8AM_TeachersGood/Views/Profile/EditNameView.swift
@@ -22,39 +22,39 @@ struct EditNameView: View {
     @State private var userInput = ""
     
     var body: some View {
-        NavigationStack {
-            VStack {
-                ZStack(alignment: .trailing) {
-                    TextField(
-                        "Type name here...",
-                        text: $userInput,
-                        prompt: Text("Type name here...")
-                            .foregroundStyle(.gray)
-                    )
-                    .disableAutocorrection(true)
-                    .textFieldStyle(OvalTextFieldStyle())
-                    if !userInput.isEmpty {
-                        Button(action: {
-                            userInput = ""
-                        }) {
-                            Image(systemName: "xmark.circle.fill")
-                                .foregroundColor(.gray)
-                        }
-                        .buttonStyle(.borderless)
-                        .padding(.trailing, 15)
-                        .transition(.scale)
+        VStack {
+            ZStack(alignment: .trailing) {
+                TextField(
+                    "Type name here...",
+                    text: $userInput,
+                    prompt: Text("Type name here...")
+                        .foregroundStyle(.gray)
+                )
+                .disableAutocorrection(true)
+                .textFieldStyle(OvalTextFieldStyle())
+                if !userInput.isEmpty {
+                    Button(action: {
+                        userInput = ""
+                    }) {
+                        Image(systemName: "xmark.circle.fill")
+                            .foregroundColor(.gray)
                     }
+                    .buttonStyle(.borderless)
+                    .padding(.trailing, 15)
+                    .transition(.scale)
                 }
-                .background(Color.white)
-                .cornerRadius(25)
-                .shadow(color: Color.appProfileShadow.opacity(0.4), radius: 10, x: 0, y: 4)
-                Spacer()
             }
-            .background(Color.appBackground)
-            .padding(20)
-            .navigationTitle("Name")
-            .navigationBarTitleDisplayMode(.inline)
+            .background(Color.white)
+//            .environment(\.colorScheme, .light)
+            .cornerRadius(25)
+            .compositingGroup()
+            .shadow(color: Color.appProfileShadow.opacity(0.4), radius: 10, x: 0, y: 4)
+            Spacer()
         }
+        .padding(20)
+        .background(Color.appBackground)
+        .navigationTitle("Name")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
